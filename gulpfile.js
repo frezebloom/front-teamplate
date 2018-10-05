f38089a56bdca7cbc2589e0eba870ea57c992a87
juxx <juxx@bk.ru>
2018-10-05 14:42:51 +0300

    v3

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const autoprefixer = require("gulp-autoprefixer");
 const cssnano = require("gulp-cssnano");
 const browserSync = require("browser-sync");
 const plumber = require("gulp-plumber");
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const minify = require("gulp-babel-minify");
 
 gulp.task("scss", () => {
   return gulp
@@ -20,6 +23,20 @@ gulp.task("scss", () => {
     .pipe(browserSync.reload({ stream: true }));
 });
 
+gulp.task("scripts", () => 
+  gulp.src('dev/js/**/*.js')
+  .pipe(babel({
+      presets: ['@babel/env']
+  }))
+  .pipe(concat("scripts.js"))
+  .pipe(minify({
+    mangle: {
+      keepClassName: true
+    }
+  }))
+  .pipe(gulp.dest('dist/js'))
+);
+
 gulp.task("browser-sync", () => {
   browserSync({
     server: {
@@ -29,7 +46,8 @@ gulp.task("browser-sync", () => {
   });
 });
 
-gulp.task("default", ["browser-sync", "scss"], () => {
+gulp.task("default", ["browser-sync", "scss", "scripts"], () => {
   gulp.watch("dev/scss/**/*.scss", ["scss"]);
+  gulp.watch("dev/js/**/*.js", ["scripts"]);
   gulp.watch("dist/*.html", browserSync.reload);
 });
